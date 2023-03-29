088c8437489ab24ea797be853fd2a9a81a046f9b
xuxingan <xuxingan@agilestar.cn>
2023-03-29 17:05:55 +0800

    test

--- gen_correct_answer.js
+++ gen_correct_answer.js
@@ -2,4 +2,4 @@ let res = '';
 for (let i =0;i<document.getElementsByClassName('answer-ansys').length;i++ ){
     res += 'document.getElementById("q'+(i+2)+'").value='+'\''+document.getElementsByClassName('answer-ansys')[i].innerText.substr(document.getElementsByClassName('answer-ansys')[i].innerText.indexOf('\n')+1,document.getElementsByClassName('answer-ansys')[i].length)+'\'\n'
 }
-console.log(res)
+console.log("结果"+res)
